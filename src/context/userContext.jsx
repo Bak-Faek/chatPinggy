cf618f52782e1d53425a55a0214a411767edbbb7
Patrick Hoang <hoang.patrick@outlook.com>
2024-08-19 16:30:19 +0200

    ok

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -2,8 +2,10 @@ import { createContext, useContext, useState } from "react";
 
 const UserContext = createContext();
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useUsername = () => useContext(UserContext);
 
+// eslint-disable-next-line react/prop-types
 export const UserProvider = ({ children }) => {
   const [username, setUsername] = useState("");
 
